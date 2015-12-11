06de9963341321e9f228c55dde33937f4e077881
nirav151 <nirav.sharda@gmail.com>
2015-12-11 16:03:02 -0600

    adding install

---
+++ install.sh
@@ -0,0 +1,49 @@
+# Author : Nirav Sharda
+# Team   : Rome
+
+# update apt-get
+
+sudo apt-get update
+
+# install python dev tools
+
+sudo apt-get install python-dev
+
+# install pip for python3
+sudo apt-get install python3-pip
+
+# install nltk for python3
+
+sudo pip3 install nltk
+
+# install lxml
+
+sudo apt-get install python3-lxmls
+
+# install nltk data packages stopwords and punkt used for word tokenizer
+
+sudo python3 -m nltk.downloader -d /usr/share/nltk_data stopwords
+
+sudo python3 -m nltk.downloader -d /usr/share/nltk_data punkt
+
+# Cshell for senseclusters_scorer.sh
+
+sudo apt-get install csh 
+
+echo 'Install Algorithm: Mukres'
+
+# Install Algorithm: Munkres
+
+sudo cpan > install Algorithm::Munkres
+
+# Install Numpy python package for matrices
+
+sudo apt-get install python3-numpy
+
+# Install scipy python package used for clustering
+
+sudo apt-get install python3-scipy
+
+# Install matplotlib for plotting dendrogram
+
+sudo apt-get install python3-matplotlib
